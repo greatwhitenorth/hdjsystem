76e955fe87042b99107b4630fa3b3744fac507bc
greatwhitenorth <hughjenn@gmail.com>
2017-04-19 10:34:44 -0700

    adding a basic distutils setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Extension
+
+setup(name='System tools for Hugh Jennings',
+      version='0.1',
+      description='Linux system tools',
+      author='Hugh Jennings',
+      author_email='hughjenn@gmail.com',
+      packages=['hdjLog','process'],)
